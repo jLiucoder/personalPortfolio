2383351f168f720d6bdde9d83b62353c44994155
jLiucoder <55926282+jLiucoder@users.noreply.github.com>
2024-04-16 19:23:16 -0400

    minor fix

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,22 +1,26 @@
 import React, { Component } from 'react';
 
 class Header extends Component {
-  render() {
+	render() {
+		if (this.props.data) {
+			var name = this.props.data.name;
+			var occupation = this.props.data.occupation;
+			var description = this.props.data.description;
+			var city = this.props.data.address.city;
+			var networks = this.props.data.social.map(function (network) {
+				return (
+					<li key={network.name}>
+						<a href={network.url}>
+							<i className={network.className}></i>
+						</a>
+					</li>
+				);
+			});
+		}
 
-    if(this.props.data){
-      var name = this.props.data.name;
-      var occupation= this.props.data.occupation;
-      var description= this.props.data.description;
-      var city= this.props.data.address.city;
-      var networks= this.props.data.social.map(function(network){
-        return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
-      })
-    }
-
-    return (
-      <header id="home">
-
-      <nav id="nav-wrap">
+		return (
+			<header id="home">
+				{/* <nav id="nav-wrap">
 
          <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
 	      <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a>
@@ -26,30 +30,27 @@ class Header extends Component {
             <li><a className="smoothscroll" href="#about">About</a></li>
 	         <li><a className="smoothscroll" href="#resume">Resume</a></li>
             <li><a className="smoothscroll" href="#portfolio">Works</a></li> */}
-            {/* <li><a className="smoothscroll" href="#testimonials">Testimonials</a></li>
+				{/* <li><a className="smoothscroll" href="#testimonials">Testimonials</a></li>
             <li><a className="smoothscroll" href="#contact">Contact</a></li> */}
-         </ul>
+				{/* </ul> */}
 
-      </nav>
+				{/* // </nav> */}
 
-      <div className="row banner">
-         <div className="banner-text">
-            <h1 className="responsive-headline">{name}</h1>
-            <h3> {description}.</h3>
-            <hr />
-            <ul className="social">
-               {networks}
-            </ul>
-         </div>
-      </div>
+				<div className="row banner">
+					<div className="banner-text">
+						<h1 className="responsive-headline">{name}</h1>
+						<h3> {description}.</h3>
+						<hr />
+						<ul className="social">{networks}</ul>
+					</div>
+				</div>
 
-      {/* <p className="scrolldown">
+				{/* <p className="scrolldown">
          <a className="smoothscroll" href="#about"><i className="icon-down-circle"></i></a>
       </p> */}
-
-   </header>
-    );
-  }
+			</header>
+		);
+	}
 }
 
 export default Header;
