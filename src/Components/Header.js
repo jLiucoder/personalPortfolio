65eac7da7124b82a990c1a1c098df1c8a06d7e8a
jLiucoder <55926282+jLiucoder@users.noreply.github.com>
2024-04-16 18:09:12 -0400

    data change

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -22,10 +22,10 @@ class Header extends Component {
 	      <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a>
 
          <ul id="nav" className="nav">
-            <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
+            {/* <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
             <li><a className="smoothscroll" href="#about">About</a></li>
 	         <li><a className="smoothscroll" href="#resume">Resume</a></li>
-            {/* <li><a className="smoothscroll" href="#portfolio">Works</a></li> */}
+            <li><a className="smoothscroll" href="#portfolio">Works</a></li> */}
             {/* <li><a className="smoothscroll" href="#testimonials">Testimonials</a></li>
             <li><a className="smoothscroll" href="#contact">Contact</a></li> */}
          </ul>
@@ -43,9 +43,9 @@ class Header extends Component {
          </div>
       </div>
 
-      <p className="scrolldown">
+      {/* <p className="scrolldown">
          <a className="smoothscroll" href="#about"><i className="icon-down-circle"></i></a>
-      </p>
+      </p> */}
 
    </header>
     );
